cd94fc5ef4e43a006d7bc6a58c288d3bd813a80f
Anas Jehani <creativelybankrupt02@gmail.com>
2021-10-10 19:31:33 +0100

    yea

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-console.log("pls work")
+import './style.css';
+import restaurant from './restaurant.jpeg';
+import xmen from './xmen.png';
+let content=document.querySelector('#content')
+let nav=document.createElement('nav')
+document.body.prepend(nav)
+const logo=new Image()
+logo.src=xmen;
+nav.appendChild(logo)
+let Home=document.createElement("a")
+let Menu=document.createElement("a")
+let About=document.createElement("a")
+let array=[Home,Menu,About]
+for(let i=0;i<=2;i++){
+    nav.appendChild(array[i])
+    array[i].setAttribute('class','nav')
+}
+let footer=document.createElement('footer')
+document.body.appendChild(footer)
+Home.textContent="Home"
+Menu.textContent="Menu"
+Menu.href="../dist/menu.html"
+About.textContent="About"
+content.textContent="helo"
+const background=new Image();
+background.src=restaurant;
+content.textContent="This is a x-men themed restaurant, prolly the first ever. Enjoy."
+footer.innerHTML="<a href='https://github.com/Ajehani'>More Content</a>"
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,33 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  // plugins: [
+  //   new HtmlWebpackPlugin({
+  //     title: 'Output Management',
+  //   }),
+  // ],
   output: {
-    filename: 'main.js',
+    filename: './main.js',
     path: path.resolve(__dirname, 'dist'),
+    // clean:true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
   },
 };
